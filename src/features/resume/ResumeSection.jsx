2d15d81fb21ea3d55f41b2e5a2d8af122a8b22a4
David Charatan <david.charatan@gmail.com>
2021-04-24 20:44:33 -0400

    Add hash links to resume page.

--- src/features/resume/ResumeSection.jsx
+++ src/features/resume/ResumeSection.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 import PropTypes from 'prop-types';
 
-const ResumeSection = ({ name, last, children }) => (
+const ResumeSection = ({ name, last, children, id }) => (
   <div className={last ? '' : 'mb-4'}>
-    <h2 className="resume">{name}</h2>
+    <HashLink id={id}>
+      <h2 className="resume">{name}</h2>
+    </HashLink>
     {children}
   </div>
 );
 
 ResumeSection.propTypes = {
   name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   last: PropTypes.bool,
   children: PropTypes.node,
 };
