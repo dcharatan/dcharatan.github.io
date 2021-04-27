81c3a6f264d8d4f4360bee795c5197248fd4eda1
David Charatan <david.charatan@gmail.com>
2021-04-26 20:51:43 -0400

    Updates

--- src/features/projects/dynamic-camera-relocalization/DynamicCameraLocalization.js
+++
@@ -1,8 +0,0 @@
-const title = 'Simulated Implementation of Dynamic Camera Relocalization Paper';
-const description = `An implementation of Feng et al.’s paper *6D Dynamic Camera Relocalization from Single Reference Image* for ENGN 2560: Computer Vision`;
-
-export default {
-  title,
-  description,
-  href: 'https://github.com/dcharatan/dcr-reimplementation',
-};
