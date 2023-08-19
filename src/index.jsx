8658fe0e46a9e2ae179a265a6c3c5eea91de2507
David Charatan <david.charatan@gmail.com>
2023-08-19 11:13:37 -0700

    Fix deprecation thing.

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
